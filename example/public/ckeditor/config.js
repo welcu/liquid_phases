775a69a3277952f0430fb3bc1935987607631743
Álvaro <yo@aquezada.cl>
2011-12-29 13:08:13 -0300

    cambios

--- example/public/ckeditor/config.js
+++ example/public/ckeditor/config.js
@@ -3,5 +3,27 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-// Define changes to default configuration here. For example:
-// config.language = 'fr';
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	config.uiColor = '#FF0000';
+	config.toolbar = 'Phases';
+  config.toolbarCanCollapse = false;
+	config.toolbar_Phases = [
+    ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+    ['Undo','Redo'],
+    ['Link','Unlink'],
+    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+    ['RemoveFormat'],
+    ['Source'],
+    '/',
+    ['Bold','Italic','Underline'],
+    ['NumberedList','BulletedList'],
+    ['Image','Table'],
+    ['Format','Font','FontSize'],
+    ['TextColor','BGColor']
+  ];
+
+	config.tabSpaces = 4;
+
+};
